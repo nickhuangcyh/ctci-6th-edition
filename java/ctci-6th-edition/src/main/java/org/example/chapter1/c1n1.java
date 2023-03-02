37de08c9c4e18317803fee00eb752fbf387809c8
NickHuang <nick25932219@gmail.com>
2023-03-02 18:27:54 +0800

    [feature] 1.1 completed

---
+++ java/ctci-6th-edition/src/main/java/org/example/chapter1/c1n1.java
@@ -0,0 +1,51 @@
+package org.example.chapter1;
+
+/*
+1.1 Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+Hints: #44, #117, #132
+
+## 44.
+Try a hash table.
+
+## 117.
+Could a bit vector be useful?
+
+## 132.
+Can you solve it in O(N log N) time? What might a solution like that look like?
+*/
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class c1n1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        List<String> testStrings = new ArrayList() {{
+            add("abcdefghijklmnopqrstuvwxyz");
+            add("abcdeef");
+        }};
+        for (Integer i = 0; i < testStrings.size(); i++) {
+            String testString = testStrings.get(i);
+            System.out.println("[Output] " + isUniqueChars(testString));
+        }
+    }
+
+    static Boolean isUniqueChars(String str) {
+        System.out.println("[Input] " + str);
+
+        Map<Character, Boolean> charMap = new HashMap();
+        for (int i = 0; i < str.length(); i++) {
+            Boolean hasAlphabet = charMap.get(str.charAt(i));
+            if (hasAlphabet != null) {
+                return false;
+            }
+            charMap.put(str.charAt(i), true);
+        }
+
+        return true;
+    }
+}
